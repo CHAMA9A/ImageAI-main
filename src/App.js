12dec0831f81eab6286513f3db2b6e2950f66833
Mohamed Ali JAMMAA <cards_2002@hotmail.fr>
2024-10-03 01:10:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ImagesGenerator from './Component/ImagesGenerator/ImageGenerator';
+
+function App() {
+  return (
+    <div>
+      <ImagesGenerator/>
+    </div>
+  );
+}
+
+export default App;
